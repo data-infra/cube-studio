4d7d686fa7fb28296c78a863cfb40d28f90f6089
data-infra <825485697@qq.com>
2024-07-05 14:11:18 +0800

    添加模板制作示例

--- myapp/vision/src/pages/Home.tsx
+++ myapp/vision/src/pages/Home.tsx
@@ -165,6 +165,12 @@ const Home: React.FC = () => {
         img: '/static/assets/images/ad/video-cover1-thumb.png',
         url: 'https://cube-studio.oss-cn-hangzhou.aliyuncs.com/cube-studio.mp4',
         type: 'video',
+      },
+      {
+        name: t('自定义任务模板'),
+        img: '/static/assets/images/ad/video-cover2-thumb.png',
+        url: 'https://cube-studio.oss-cn-hangzhou.aliyuncs.com/job-template.mp4',
+        type: 'video',
       }
     ]);
     api.pipeline_modelview_demo().then((res: any) => {
--- myapp/vision/src/static/home.ts
+++ myapp/vision/src/static/home.ts
@@ -6,5 +6,11 @@ export const videoDemo = [
     img: '/static/assets/images/ad/video-cover1-thumb.png',
     url: 'https://cube-studio.oss-cn-hangzhou.aliyuncs.com/cube-studio.mp4',
     type: 'video',
+  },
+  {
+    name: '自定义任务模板',
+    img: '/static/assets/images/ad/video-cover2-thumb.png',
+    url: 'https://cube-studio.oss-cn-hangzhou.aliyuncs.com/job-template.mp4',
+    type: 'video',
   }
 ];
